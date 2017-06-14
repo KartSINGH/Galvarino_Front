c66a60896e36365a835dbabdd7a4c94ab35583dd
kartik singh <kart.singh15@gmail.com>
2017-06-14 12:32:39 +0530

    Loading ANimations

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
   "particles": {
     "number": {
-      "value": 40,
+      "value": 80,
       "density": {
         "enable": true,
         "value_area": 946.9771699587272
